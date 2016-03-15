7277dd24f2979014d6c9eb2ffdc45f39b83ff7a0
androiduser159 <stannum72@gmail.com>
2016-03-16 00:21:35 +0400

    commit1

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def application(environ,start_response):
-	resp="\r\n".join(environ['QUERY_STRING'].split("&"))
+	resp="\r\n".join(environ['QUERY_STRING'].split("&"))+"\r\n"
 	status='200 OK'
 	headers=[('Content-Type','text/plain')]
 	start_response(status,headers)

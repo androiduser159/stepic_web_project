4dfe2abb0ad11d283ecb50c39241521d9bfa6b0c
androiduser159 <stannum72@gmail.com>
2016-03-19 00:06:43 +0400

    commit2

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 def application(environ,start_response):
 	resp="\r\n".join(environ['QUERY_STRING'].split("&"))+"\r\n"
+	#resp=[item + "\r\n" for item in environ['QUERY_STRING'].split(&)]
 	status='200 OK'
 	headers=[('Content-Type','text/plain')]
 	start_response(status,headers)
--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 #sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.conf
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/gunicorn restart
 #sudo gunicorn -b 0.0.0.0:8080 hello
 #sudo ln -sf /home/box/web/hello.py /usr/local/lib/python2.7/hello.py
